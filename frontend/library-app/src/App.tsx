c82add79d4deef64664f027208700151003e4142
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-19 01:23:26 +0600

    Created a layout component named ExploreTopBooks and added npm bootstrap.

--- frontend/library-app/src/App.tsx
+++ frontend/library-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./layouts/NavbarFooter/Navbar";
+import {ExploreTopBooks} from "./layouts/HomePage/ExploreTopBooks";
 
 function App() {
     return (
         <>
             <Navbar/>
+            <ExploreTopBooks/>
         </>
     );
 }
