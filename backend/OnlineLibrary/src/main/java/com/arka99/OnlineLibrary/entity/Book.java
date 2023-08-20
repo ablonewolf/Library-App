db6952ff2514a82bc25e8fe90b237882b4b4a071
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-20 17:48:00 +0600

    created the base entity and book entity classes.

---
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/entity/Book.java
@@ -0,0 +1,25 @@
+package com.arka99.OnlineLibrary.entity;
+
+import com.arka99.OnlineLibrary.common.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "book")
+public class Book extends BaseEntity {
+    private String title;
+    private String author;
+    private String description;
+    private Integer copies;
+    private Integer copiesAvailable;
+    private String category;
+    private String img;
+}
