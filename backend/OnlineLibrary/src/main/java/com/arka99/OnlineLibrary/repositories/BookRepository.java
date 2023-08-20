f8bd33dfff9f8920c8e36c2ac23fc10bba37f8dd
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-20 18:00:08 +0600

    created repository interface for book entity.

---
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package com.arka99.OnlineLibrary.repositories;
+
+import com.arka99.OnlineLibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
