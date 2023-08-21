3bf3a67c212a558cb9e3c4d70f5295f918fb856a
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-21 11:00:43 +0600

    Set up configuration for read-only api.

---
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/configuration/RestDataConfig.java
@@ -0,0 +1,41 @@
+package com.arka99.OnlineLibrary.configuration;
+
+import com.arka99.OnlineLibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestDataConfig implements RepositoryRestConfigurer {
+    private final String allowedOrigin = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+                                                     CorsRegistry cors) {
+        HttpMethod[] unsupportedMethods = {
+            HttpMethod.POST,
+            HttpMethod.PATCH,
+            HttpMethod.DELETE,
+            HttpMethod.PUT};
+        config.exposeIdsFor(Book.class);
+        disableHttpMethods(Book.class, config, unsupportedMethods);
+
+//        configure CORS mapping
+        cors.addMapping(config.getBasePath() + "**/")
+            .allowedOrigins(allowedOrigin);
+    }
+
+    private void disableHttpMethods(Class book,
+                                    RepositoryRestConfiguration config,
+                                    HttpMethod[] unsupportedMethods) {
+        config.getExposureConfiguration()
+            .forDomainType(book)
+            .withItemExposure(((metdata, httpMethods) ->
+                httpMethods.disable(unsupportedMethods)))
+            .withCollectionExposure(((metdata, httpMethods) ->
+                httpMethods.disable(unsupportedMethods)));
+    }
+
+}
--- backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/repositories/BookRepository.java
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.arka99.OnlineLibrary.repositories;
 
 import com.arka99.OnlineLibrary.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
