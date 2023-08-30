aa845afb4e3bb72a07a0eca435d9f4baadb8610d
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-30 11:48:47 +0600

    enabled routing for homepage and search page across all components.

--- frontend/library-app/src/App.tsx
+++ frontend/library-app/src/App.tsx
@@ -3,12 +3,24 @@ import './App.css';
 import {Navbar} from "./layouts/NavbarFooter/Navbar";
 import {Footer} from "./layouts/NavbarFooter/Footer";
 import {HomePage} from "./layouts/HomePage/HomePage";
+import {Redirect, Route, Switch} from "react-router-dom";
+import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
 
 export const App = () => {
     return (
         <>
             <Navbar/>
-            <HomePage/>
+            <Switch>
+                <Route path='/' exact>
+                    <Redirect to='/home'/>
+                </Route>
+                <Route path='/home'>
+                    <HomePage/>
+                </Route>
+                <Route path='/search'>
+                    <SearchBooksPage/>
+                </Route>
+            </Switch>
             <Footer/>
         </>
     );
