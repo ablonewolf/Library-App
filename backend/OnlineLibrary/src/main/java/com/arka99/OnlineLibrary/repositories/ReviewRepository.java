6e7e4df6ecc2544f68e280e7aaf430653266e13b
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-09-04 17:23:38 +0600

    created repository method to find reviews by book ID.

--- backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/repositories/ReviewRepository.java
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/repositories/ReviewRepository.java
@@ -1,9 +1,13 @@
 package com.arka99.OnlineLibrary.repositories;
 
 import com.arka99.OnlineLibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Page<Review> findReviewByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
 }
