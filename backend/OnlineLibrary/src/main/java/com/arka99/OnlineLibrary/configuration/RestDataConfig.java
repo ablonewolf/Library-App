52692b8c05806320c63599f4ad14e9d22f0addbb
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-09-04 17:03:43 +0600

    Created Repository interface for Review and added configuration for Review in RestDataConfig class.

--- backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/configuration/RestDataConfig.java
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/configuration/RestDataConfig.java
@@ -1,6 +1,7 @@
 package com.arka99.OnlineLibrary.configuration;
 
 import com.arka99.OnlineLibrary.entity.Book;
+import com.arka99.OnlineLibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -19,19 +20,22 @@ public class RestDataConfig implements RepositoryRestConfigurer {
             HttpMethod.PATCH,
             HttpMethod.DELETE,
             HttpMethod.PUT};
-        config.exposeIdsFor(Book.class);
+
+        config.exposeIdsFor(Book.class, Review.class);
+
         disableHttpMethods(Book.class, config, unsupportedMethods);
+        disableHttpMethods(Review.class, config, unsupportedMethods);
 
 //        configure CORS mapping
         cors.addMapping(config.getBasePath() + "/**")
             .allowedOrigins(allowedOrigin);
     }
 
-    private void disableHttpMethods(Class book,
+    private void disableHttpMethods(Class objectClass,
                                     RepositoryRestConfiguration config,
                                     HttpMethod[] unsupportedMethods) {
         config.getExposureConfiguration()
-            .forDomainType(book)
+            .forDomainType(objectClass)
             .withItemExposure(((metdata, httpMethods) ->
                 httpMethods.disable(unsupportedMethods)))
             .withCollectionExposure(((metdata, httpMethods) ->
---
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/repositories/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.arka99.OnlineLibrary.repositories;
+
+import com.arka99.OnlineLibrary.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
