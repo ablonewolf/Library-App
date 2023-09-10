2e02a193576549c468fe9f04becc19d66ee53168
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-09-10 15:09:36 +0600

    added okta package and configured authentication using okta, used the default login page by okta.

---
+++ frontend/library-app/src/Auth/LoginWidget.jsx
@@ -0,0 +1,29 @@
+import {useOktaAuth} from "@okta/okta-react";
+import {SpinnerLoading} from "../utils/SpinnerLoading";
+import {Redirect} from "react-router-dom";
+import {OktaSignInWidget} from "./OktaSignInWidget";
+
+export const LoginWidget = ({config}) => {
+    const {oktaAuth, authState} = useOktaAuth();
+    const onSuccess = (tokens) => {
+        oktaAuth.handleLoginRedirect(tokens);
+    }
+
+    const onError = (err) => {
+        console.log(err)
+    }
+
+    if (!authState) {
+        return (
+            <SpinnerLoading/>
+        );
+    }
+    return authState.isAuthenticated ?
+        <Redirect to={{pathname: '/'}}/>
+        :
+        <OktaSignInWidget
+            config={config}
+            onSuccess={onSuccess}
+            onError={onError}
+        />
+}
