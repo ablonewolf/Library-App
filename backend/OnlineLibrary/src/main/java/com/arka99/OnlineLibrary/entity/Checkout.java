5dc7289786707ef6bbd6bfd036f63a7b27d88eaf
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-09-10 17:33:52 +0600

    Created the Entity for Checkout.

---
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/entity/Checkout.java
@@ -0,0 +1,33 @@
+package com.arka99.OnlineLibrary.entity;
+
+import com.arka99.OnlineLibrary.common.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "checkout")
+public class Checkout extends BaseEntity {
+    @Column(name = "user_email")
+    private String userEmail;
+    @Column(name = "checkout_date")
+    private String checkoutDate;
+    @Column(name = "return_date")
+    private String returnDate;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+}
