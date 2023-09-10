82777ebc81bfd009b325e39be42feddda7a244c6
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-09-10 17:39:07 +0600

    Created the repository for Checkout

---
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/dao/CheckoutRepository.java
@@ -0,0 +1,10 @@
+package com.arka99.OnlineLibrary.dao;
+
+import com.arka99.OnlineLibrary.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+    Checkout findCheckoutByUserEmailAndBookId(String userEmail, Long bookId);
+}
