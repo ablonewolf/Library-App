51c9384e83c34ad4b4729c51a971830a45d27a0e
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-09-11 13:43:03 +0600

    Added builder for each entity classes.

--- backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/entity/Book.java
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/entity/Book.java
@@ -4,11 +4,13 @@ import com.arka99.OnlineLibrary.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
--- backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/entity/Review.java
+++ backend/OnlineLibrary/src/main/java/com/arka99/OnlineLibrary/entity/Review.java
@@ -9,6 +9,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import lombok.Setter;
 import java.util.Date;
 
 @Entity
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
