ba1f41e44cd0f404c24f16aa7643ddca6c77a35e
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-09-12 09:53:56 +0600

    Initiated sql scripts for checkout.

---
+++ SQL_Scripts/checkout.sql
@@ -0,0 +1,3 @@
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (1, 'arka.bhuiyan@gmail.com', '2023-09-11', '2023-09-18', 4);
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (2, 'newaz.sharif@gmail.com', '2023-09-11', '2023-09-18', 2);
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (3, 'arka.bhuiyan@gmail.com', '2023-09-11', '2023-09-18', 1);
