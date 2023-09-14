aa04ca28e3bb8009408644a98c803c4aa9f65235
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-09-14 12:56:33 +0600

    Created the function fetchUserCurrentCheckoutBookCount to consume the API to count the number of Check out books count of a logged-in user.

---
+++ frontend/library-app/src/APIConsumMethods/fetchUserCurrentCheckoutBookCount.ts
@@ -0,0 +1,23 @@
+import {BaseURL} from "../models/constants/BaseURL";
+import {AuthState} from "@okta/okta-auth-js/lib/types/AuthState"
+export const fetchUserCurrentCheckoutBookCount = async (authState: AuthState | null,
+                                                        setCurrentCheckoutBookCount: (count: number) => void,
+                                                        setIsLoadingCurrentBookCheckoutCount: (isLoading: boolean) => void) => {
+    if (authState && authState.isAuthenticated) {
+        const apiURL = `${BaseURL}/books/secure/currentCheckouts/count`;
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+        const currentBooksCheckoutCountResponse = await fetch(apiURL, requestOptions);
+        if (!currentBooksCheckoutCountResponse.ok) {
+            throw new Error("Something went wrong fetching currentBooks checkout count for this user.");
+        }
+        const currentBooksCheckoutCountJson = await currentBooksCheckoutCountResponse.json();
+        setCurrentCheckoutBookCount(currentBooksCheckoutCountJson);
+    }
+    setIsLoadingCurrentBookCheckoutCount(false);
+}
