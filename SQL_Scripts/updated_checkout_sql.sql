ed198431b6d9ae5786e6371033eab4c1ee08af66
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-09-14 17:02:21 +0600

    added updated sql scripts for book and checkouts.

---
+++ SQL_Scripts/updated_checkout_sql.sql
@@ -0,0 +1,8 @@
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (1, 'newaz.sharif@gmail.com', '2023-09-11', '2023-09-18', 2);
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (2, 'arka.bhuiyan@gmail.com', '2023-09-11', '2023-09-18', 1);
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (3, 'arka.bhuiyan@gmail.com', '2023-09-13', '2023-09-20', 5);
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (5, 'newaz.sharif@gmail.com', '2023-09-13', '2023-09-20', 8);
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (6, 'abul.bashar@gmail.com', '2023-09-14', '2023-09-21', 1);
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (7, 'abul.bashar@gmail.com', '2023-09-14', '2023-09-21', 4);
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (8, 'nipa.howladar@gmail.com', '2023-09-14', '2023-09-21', 4);
+INSERT INTO onlineLibraryDatabase.checkout (id, user_email, checkout_date, return_date, book_id) VALUES (9, 'nipa.howladar@gmail.com', '2023-09-14', '2023-09-21', 5);
